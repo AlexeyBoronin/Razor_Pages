847cb730172c8416f4254ca764fa09f1e02e0326
Алексей Боронин <boronin_1993@mail.ru>
2024-05-13 19:25:50 +0300

    1

--- Razor_Pages/Pages/Index.cshtml.cs
+++ Razor_Pages/Pages/Index.cshtml.cs
@@ -11,11 +11,24 @@ namespace Razor_Pages.Pages
         public List<User> Users { get; private set; } = new();
         public IndexModel(ApplicationContext db)
         {
-            context = db; 
+            context = db;
         }
         public void OnGet()
         {
-            Users=context.Users.AsNoTracking().ToList();
+            Users = context.Users.AsNoTracking().ToList();
+        }
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+
+            if (user != null)
+            {
+                context.Users.Remove(user);
+                await context.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
         }
     }
 }
+
